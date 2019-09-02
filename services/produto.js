537bf72860f9b514171bbf4786310252d1ae001a
SouBia <bi.rcs.96@gmail.com>
2019-09-02 19:29:57 -0300

    atualização da API com put e Delete

--- services/produto.js
+++ services/produto.js
@@ -1,23 +1,30 @@
-const produtos = [
+let idInicial = 1
+
+function autoID() {
+    return idInicial++
+}
+
+let produtos = [
     {
-        id: 1,
+        id: autoID(),
         nome: 'Samsung Galax S10',
         fabricante: 'Samgung',
         preco: 4000,
     },
     {
-        id:2,
+        id:autoID(),
         nome: 'MI 9',
         fabricante: 'Xiaomi',
         preco: 1500,
     },
     {
-        id:3,
+        id:autoID(),
         nome: 'Iphone X',
         fabricante: 'Apple',
         preco: 9000,
     }
 ]
+
 const getProduto =  (id) => {
     if (!id){
         return produtos
@@ -28,10 +35,32 @@ const getProduto =  (id) => {
     }
 }
 
-const postProduto = (obj) => {
-    produtos.push(obj)
-    return obj 
+const postProduto = obj => {
+    let novoObj = {
+        id: autoID(),
+        ...obj
+    }
+    produtos.push(novoObj)
+    return novoObj
 }
 
+const deleteProduto = id => {
+    let produtosSelecionados = produtos.filter(produto => produto.id != id)
+    let produtoDeletado = produtos.filter(produto => produto.id == id)
+    produtos = produtosSelecionados
+    return produtoDeletado
+}
 
-module.exports = { getProduto, postProduto }
+const alterarProduto = (id, dados) => {
+    const index = produtos.findIndex(produto => produto.id == id)
+    if (index < 0) return null    
+    const produtoAlterado = {
+        ...produtos[index],
+        ...dados
+    }
+
+    produtos.splice(index, 1, produtoAlterado)
+    return produtoAlterado
+}
+
+module.exports = { getProduto, postProduto, deleteProduto, alterarProduto }
